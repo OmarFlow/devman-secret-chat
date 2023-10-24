2b46f32e5c9f50d25d3539b9a71a0452439920be
Serov Roman <academboyhtml@gmail.com>
2023-10-24 15:54:38 +0300

    Add CLI options.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
+import argparse
 import asyncio
 from datetime import datetime
 
 import aiofiles
 
 
-async def chat_spy():
+async def chat_spy(host, port, log_file):
     reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        host, port)
+    print("!!!")
     try:
         while not reader.at_eof():
             data = await reader.read(500)
-            async with aiofiles.open('chat_log.txt', mode='a') as f:
+            async with aiofiles.open(log_file, mode='a') as f:
                 write_str = f'{datetime.now().strftime("[%d.%m.%y %H:%M]")} {data.decode(errors="ignore")}'
                 await f.write(write_str)
             print(f'Received: {data.decode(errors="ignore")!r}')
     except ConnectionError:
         print("Network error")
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", help="specify the host", type=str)
+    parser.add_argument("--port", help="specify the port", type=int)
+    parser.add_argument("--history", help="path to log file", type=str)
+    args = parser.parse_args()
 
-asyncio.run(chat_spy())
+    if args.host is None or args.port is None or args.history is None:
+        raise Exception("Specify all args!")
+
+    asyncio.run(chat_spy(args.host, args.port, args.history))
